c0d7bfa473e6bd1cf654edd68019cd5504005ad4
Gustaf26 <gustavo.catalasverdrup@gmail.com>
2025-04-29 18:49:53 +0200

    Synchronizing the wek calendar with the month option so 1st of each month is displayed when changing month. Working with some flaws

--- frontend/booking-frontend/src/components/calendars/subcalendars/WeekCalendar.jsx
+++ frontend/booking-frontend/src/components/calendars/subcalendars/WeekCalendar.jsx
@@ -9,8 +9,6 @@ import KeyboardArrowRightIcon from '@mui/icons-material/KeyboardArrowRight';
 
 let todaysWeekDayNr = new Date().getDay()
 
-const thisMonth = new Date().getMonth()
-
 const todaysDate = new Date().getDate()
 
 const DayCell = (props) => {
@@ -30,7 +28,6 @@ const DayCell = (props) => {
         e.target.classList.add('active')
     }
 
-
     return (<div key={"week-calendar-day" + weekDayNr} onClick={(e) => activateDay(e)}
         className={weekDayNr === todaysWeekDayNr && props.dayDate === todaysDate ?
             "week-calendar-day today" : "week-calendar-day"}>
@@ -45,7 +42,7 @@ const DayCell = (props) => {
         })} */}
 
         <span className={weekDayNr === props.todaysWeekDayNr && props.dayDate == todaysDate ? "week-cal-day-nr today" : "week-cal-day-nr"}>
-            {allWeekDaysName[weekDayNr - 1]}{"  "}{props.dayDate >= 1 ? props.dayDate : ''}</span>
+            {allWeekDaysName[weekDayNr - 1]}{"  "}{props.dayDate >= 1 && props.dayDate <= props.monthDays ? props.dayDate : ''}</span>
     </div>)
 }
 
@@ -53,37 +50,43 @@ const DayCell = (props) => {
 
 export default function WeekCalendar() {
 
-    const { events, actualMonth } = useContext(MainContext)
+    const { events, actualMonth, setActualMonth } = useContext(MainContext)
 
     const [weekCells, setWeekCells] = useState([])
     const [monthDays, setMonthDays] = useState((actualMonth) % 2 === 0 && (actualMonth) !== 2 && (actualMonth) !== 8 ? 30 : (actualMonth) % 2 === 0 ? 28 : 31)
     const [weekPlusIndex, setPlusIndex] = useState(1)
     const [weekMinusIndex, setMinusIndex] = useState(1)
+    const thisMonth = new Date().getMonth()
+    const [referenceMonday, setReferenceMonday] = useState((new Date().getDate()) - (new Date().getDay()) + 1)
 
     const updateCalendar = (action) => {
 
         let allWeekdays = []
 
-        let todaysDate = new Date().getDate()
-        let thisDayNr = new Date().getDay()
+        let referenceDate = (action === 'plus') ? referenceMonday + 7
+            : (action === 'minus') ? referenceMonday - 7
+                : (action === 'clear') ? 1 - (new Date(`2025-${actualMonth}-1`).getDay() + 1)
+                    : (new Date().getDate()) - (new Date().getDay()) + 1
 
-        let referenceDate = (action === 'plus') ? todaysDate + (weekPlusIndex * 7) : todaysDate - (weekMinusIndex * 7)
 
-        if ((referenceDate - thisDayNr) > (monthDays) || referenceDate <= -5) {
+        if ((referenceDate) > (monthDays) || referenceDate <= -6) {
             return
         }
 
+        setReferenceMonday(action !== 'clear' ? referenceDate : 1)
+
         if (action === 'plus') { setPlusIndex(prev => prev + 1); setMinusIndex(prev => prev - 1) }
-        else { setMinusIndex(prev => prev + 1); setPlusIndex(prev => prev - 1) }
+        else if (action === 'minus') { setMinusIndex(prev => prev + 1); setPlusIndex(prev => prev - 1) }
+        else setMinusIndex(1); setPlusIndex(1)
 
 
         for (let j = 0; j < 7; j++) {
 
-            let dayDate = new Date(`2025-${thisMonth}-${referenceDate - (todaysWeekDayNr - 1) + j}`).getDate()
+            let dayDate = new Date(`2025-${actualMonth}-${referenceDate + j}`).getDate()
 
             allWeekdays.push(
                 <DayCell actualMonth={actualMonth} events={events.length > 0 ? events : []} dayDate={dayDate}
-                    weekDayNr={j + 1} todaysWeekDayNr={todaysWeekDayNr} key={"weekday" + j}>
+                    monthDays={monthDays} weekDayNr={j + 1} todaysWeekDayNr={todaysWeekDayNr} key={"weekday" + j}>
                 </DayCell>
             )
         }
@@ -95,31 +98,40 @@ export default function WeekCalendar() {
 
     useEffect(() => {
 
-        let allWeekdays = []
-        let todaysDate = new Date().getDate()
-        let todaysDayNr = new Date().getDay()
+        // let allWeekdays = []
+        // let todaysDate = new Date().getDate()
+        // let todaysDayNr = new Date().getDay()
 
-        for (let j = 0; j < 7; j++) {
+        // for (let j = 0; j < 7; j++) {
 
-            let dayDate;
-
-
-            if (j < todaysWeekDayNr) dayDate = new Date(`2025-${thisMonth}-${todaysDate - (todaysWeekDayNr) + j + 1}`).getDate()
-            else if (j === todaysWeekDayNr) dayDate = new Date(`2025-${thisMonth}-${todaysDate + 1}`).getDate()
-            else dayDate = new Date(`2025-${thisMonth}-${todaysDate - todaysDayNr + j + 1}`).getDate()
-
-            allWeekdays.push(
-                <DayCell actualMonth={actualMonth} events={events.length > 0 ? events : []} dayDate={dayDate}
-                    weekDayNr={j + 1} todaysWeekDayNr={todaysWeekDayNr} key={"weekday" + j}>
-                </DayCell>
-            )
-        }
+        //     let dayDate;
 
 
-        setWeekCells(allWeekdays)
+        //     if (j < todaysWeekDayNr) dayDate = new Date(`2025-${thisMonth}-${todaysDate - (todaysWeekDayNr) + j + 1}`).getDate()
+        //     else if (j === todaysWeekDayNr) dayDate = new Date(`2025-${thisMonth}-${todaysDate + 1}`).getDate()
+        //     else dayDate = new Date(`2025-${thisMonth}-${todaysDate - todaysDayNr + j + 1}`).getDate()
+
+        //     allWeekdays.push(
+        //         <DayCell actualMonth={actualMonth} events={events.length > 0 ? events : []} dayDate={dayDate}
+        //             monthDays={monthDays} weekDayNr={j + 1} todaysWeekDayNr={todaysWeekDayNr} key={"weekday" + j}>
+        //         </DayCell>
+        //     )
+        // }
+
+        // updateCalendar('reset')
+
+        setActualMonth(thisMonth)
+        // setWeekCells(allWeekdays)
 
     }, [])
 
+
+    useEffect(() => {
+
+        if (actualMonth !== thisMonth) updateCalendar('clear')
+        else updateCalendar('reset')
+
+    }, [actualMonth])
 
 
     return (<div id="week-calendar-container">
