dc4205f04275aaba9d3ffd63af568b225ec17306
Gustaf26 <gustavo.catalasverdrup@gmail.com>
2025-05-07 15:44:05 +0200

    Event on creation modal showing with position absolute and arrows in week working fine

--- frontend/booking-frontend/src/components/calendars/MainCalendar.jsx
+++ frontend/booking-frontend/src/components/calendars/MainCalendar.jsx
@@ -2,11 +2,11 @@ import { useState, useEffect, useContext } from 'react'
 
 import MainContext from '../../contexts/MainContext.jsx'
 // import EventContext from "../../contexts/MainContext"''
-import { EventContextProvider } from '../../contexts/EventsContext.jsx'
+// import { EventContextProvider } from '../../contexts/EventsContext.jsx'
 
 import MonthCalendar from './subcalendars/MonthCalendar.jsx'
 import WeekCalendar from './subcalendars/WeekCalendar.jsx'
-import EventOnCreation from './subcalendars/EventOnCreation.jsx'
+// import EventOnCreation from './subcalendars/EventOnCreation.jsx'
 
 const MainCalendar = () => {
 
