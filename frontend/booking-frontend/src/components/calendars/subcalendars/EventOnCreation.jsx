f615217f518eb6a4b70edd56c42b8d251719a60f
Gustaf26 <gustavo.catalasverdrup@gmail.com>
2025-05-29 14:59:28 +0200

    Refactoring and commenting EventOnCreation modal

--- frontend/booking-frontend/src/components/calendars/subcalendars/EventOnCreation.jsx
+++ frontend/booking-frontend/src/components/calendars/subcalendars/EventOnCreation.jsx
@@ -6,17 +6,15 @@ import MainContext from "../../../contexts/MainContext"
 import Button from '@mui/material/Button';
 import Avatar from '@mui/material/Avatar';
 
-const thisMonth = new Date().getMonth()
-
 export default function EventOnCreation(props) {
 
     const [eventFriend, setEventFriend] = useState([])
     const [posibleContacts, setPosibleContacts] = useState([])
     const [contactsShowing, setContactsShowing] = useState(false)
 
-    const { chosenDate, chosenMonth, chosenDay, events, setEventOnCreation, contacts, actualMonth } = useContext(MainContext)
+    const { chosenDay, events, setEventOnCreation, contacts } = useContext(MainContext)
 
-    const { setEventElement, dayDate, dayNr, thisDay, day } = props
+    const { setEventElement, dayDate } = props
 
     let daysBase = [0, 7, 14, 21, 28]
 
@@ -32,6 +30,7 @@ export default function EventOnCreation(props) {
     let dayNames = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
 
 
+    // This function shows possible matches with contacts when typing in atendees input
     const showPossibleContacts = (e) => {
 
         let dummyContacts = []
@@ -51,21 +50,28 @@ export default function EventOnCreation(props) {
         setPosibleContacts(dummyContacts)
     }
 
+    const addFriendToEvent = (contact) => {
+
+        // Close the possible contacts showing
+        setPosibleContacts([]);
+        // And add a friend to event
+        setEventFriend((prev) => !prev.includes(contact) ? [...prev, contact] :
+            [...prev])
+    }
+
+    const closeModal = () => {
+        setEventElement([]);
+        setEventFriend([]);
+        setEventOnCreation(false)
+    }
+
     return (<div>
 
-        <span id="close-event-on-creation" onClick={() => {
-            setEventElement([]);
-            setEventFriend([]);
-            setEventOnCreation(false)
-        }}>
+        <span id="close-event-on-creation" onClick={closeModal}>
             X</span>
 
         <form onClick={(e) => e.stopPropagation()}>
             <div id="event-on-creation-date">
-                {/* <span className={dayNr === thisDay && thisMonth === actualMonth ?
-                    "month-cal-day-nr today" : "month-cal-day-nr"}>Event Date: {weekDays.map((days, i) => { return days.includes(dayNr) ? dayNames[i] : null })}
-                    {" "}{dayNr} of {chosenMonth}
-                </span> */}
                 {chosenDay && <span >Event Date: {weekDays.map((days, i) => { return days.includes(chosenDay) ? dayNames[i] : null })}
                 </span>}
             </div>
@@ -79,12 +85,11 @@ export default function EventOnCreation(props) {
                                 }>x</span></p>
                         })}
                         <input placeholder="Search for friend..." onChange={(e) => { showPossibleContacts(e) }}></input>
+
                         <ul id="event-creation-friends-autocomplete">
                             {posibleContacts && posibleContacts.map(contact => {
                                 return (<li onClick={() => {
-                                    setPosibleContacts([]);
-                                    setEventFriend((prev) => !prev.includes(contact) ? [...prev, contact] :
-                                        [...prev])
+                                    addFriendToEvent(contact)
                                 }}>{contact.name}<img src={contact.thumbnail} /></li>)
                             })}
                         </ul>
@@ -114,21 +119,23 @@ export default function EventOnCreation(props) {
                 <label>Event Description</label>
                 <textarea placeholder="Write event here..."></textarea>
             </div>
-            <ul className={!contactsShowing ? "event-contacts-thumbnails" : "event-contacts-thumbnails animated"} onMouseOut={() => setContactsShowing(false)}
+
+            <ul className={!contactsShowing ? "event-contacts-thumbnails" : "event-contacts-thumbnails animated"}
+                onMouseOut={() => setContactsShowing(false)}
                 onMouseOver={() => setContactsShowing(true)}>
+
                 {contactsShowing ? contacts.map((contact, i) => {
                     return (<li className="animated">
-                        <img onClick={() => {
-                            setPosibleContacts([]);
-                            setEventFriend((prev) => !prev.includes(contact) ? [...prev, contact] :
-                                [...prev])
-                        }} style={{ top: `${((i + 1) * 100) + 120}px` }}
+                        <img onClick={() => { addFriendToEvent(contact) }} style={{ top: `${((i + 1) * 100) + 120}px` }}
                             alt="contact-picture" className="on-creation-contact-thumbnail" src={contact.thumbnail} />
                     </li>)
-                }) : (<li className="dummy-avatar"><Avatar sx={{ backgroundColor: 'black', height: '40px', width: '30px' }} /></li>)}
+                }) : (<li className="dummy-avatar">
+                    <Avatar sx={{ backgroundColor: 'black', height: '40px', width: '30px' }} />
+                </li>)}
+
             </ul>
 
-            <Button onClick={() => setEventOnCreation(false)} id="new-event-button">INVITE</Button>
+            <Button onClick={closeModal} id="new-event-button">INVITE</Button>
         </form>
     </div>)
 }
--- frontend/booking-frontend/src/components/calendars/subcalendars/WeekCalendar.jsx
+++ frontend/booking-frontend/src/components/calendars/subcalendars/WeekCalendar.jsx
@@ -145,7 +145,6 @@ export default function WeekCalendar() {
             )
         }
 
-
         setWeekCells(allWeekdays)
 
     }
